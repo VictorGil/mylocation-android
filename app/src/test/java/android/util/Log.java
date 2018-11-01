fde646e5977011c98fe913e1f438b2a2267f61f8
VictorGil <victorgilsanz@gmail.com>
2018-11-01 18:32:44 +0100

    Adding test mock class for android.util.Log

---
+++ app/src/test/java/android/util/Log.java
@@ -0,0 +1,38 @@
+package android.util;
+
+/**
+ * @author Víctor Gil
+ *
+ * since November 2018
+ */
+public class Log {
+    public static int d(String tag, String msg) {
+        System.out.println("DEBUG: " + tag + ": " + msg);
+        return 0;
+    }
+
+    public static int i(String tag, String msg) {
+        System.out.println("INFO: " + tag + ": " + msg);
+        return 0;
+    }
+
+    public static int w(String tag, String msg) {
+        System.out.println("WARN: " + tag + ": " + msg);
+        return 0;
+    }
+
+    public static int e(String tag, String msg) {
+        System.out.println("ERROR: " + tag + ": " + msg);
+        return 0;
+    }
+
+    public static int wtf(String tag, String msg) {
+        System.out.println("WTF: " + tag + ": " + msg);
+        return 0;
+    }
+
+    public static int wtf(String tag, String msg, Throwable throwable) {
+        System.out.println("WTF: " + tag + ": " + msg + "\n" + throwable);
+        return 0;
+    }
+}
