7f46d8818fd8f8298ea79d4142a3563a19b14943
VictorGil <victorgilsanz@gmail.com>
2018-11-01 18:33:19 +0100

    Adding test mock class for android.widget.Toast

---
+++ app/src/test/java/android/widget/Toast.java
@@ -0,0 +1,24 @@
+package android.widget;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+/**
+ * @author Víctor Gil
+ *
+ * since September 2018
+ */
+public class Toast {
+
+    public static Toast makeText(Context context, int resourceId, int length) throws Resources.NotFoundException{
+        return new Toast();
+    }
+
+    public void setGravity(int a, int b, int c){
+
+    }
+
+    public void show(){
+
+    }
+}
